a9de6cc374a235123bfd40439208a35167514a26
neunerlei <ich@neunerlei.eu>
2025-07-09 18:00:59 +0200

    refactor: remove now unused openssl wrapper functions

--- src/OpenSsl.php
+++ src/OpenSsl.php
@@ -11,64 +11,6 @@ namespace Hawk\HawkiCrypto;
 class OpenSsl
 {
     /**
-     * @see openssl_pkey_new()
-     */
-    public function pkey_new(?array $options)
-    {
-        return openssl_pkey_new($options);
-    }
-
-    /**
-     * @see openssl_pkey_export())
-     */
-    public function pkey_export($key, &$output, ?string $passphrase = null, ?array $options = null)
-    {
-        return openssl_pkey_export($key, $output, $passphrase, $options);
-    }
-
-    /**
-     * @see openssl_pkey_get_public()
-     */
-    public function pkey_get_public($public_key)
-    {
-        return openssl_pkey_get_public($public_key);
-    }
-
-    /**
-     * @see openssl_pkey_get_private()
-     */
-    public function pkey_get_private($private_key, ?string $passphrase = null)
-    {
-        return openssl_pkey_get_private($private_key, $passphrase);
-    }
-
-    /**
-     * @see openssl_public_encrypt()
-     */
-    public function public_encrypt(
-        string $data,
-               &$encrypted_data,
-               $public_key,
-        int    $padding = OPENSSL_PKCS1_PADDING
-    )
-    {
-        return openssl_public_encrypt($data, $encrypted_data, $public_key, $padding);
-    }
-
-    /**
-     * @see openssl_private_decrypt()
-     */
-    public function private_decrypt(
-        string $data,
-               &$decrypted_data,
-               $private_key,
-        int    $padding = OPENSSL_PKCS1_PADDING
-    )
-    {
-        return openssl_private_decrypt($data, $decrypted_data, $private_key, $padding);
-    }
-
-    /**
      * @see openssl_encrypt()
      */
     public function encrypt(
